46fe7ac940209c0af8f846a149b3b3b129128387
GamuIsAlreadyTaken <GamuDiez@gmail.com>
2024-04-18 12:57:11 +0200

    changed to list macro
    It seems cleaner to be able to do it in one block

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,36 @@
 use std::ops::*;
-macro_rules! impl_op {
-    {$trait: ident < $( $bound: ident ),+ > => $imp: item} => {
-        impl<T> $trait for Coordinates<T>
-            where
-                T: $trait< $($bound),*, Output=T>,
-        {
-            type Output = Self;
-            $imp
-        }
-    };
-    {$trait: ident => $imp: item} => {
-        impl<T> $trait for Coordinates<T>
+macro_rules! impl_ops {
+    {$($trait: ident => $imp: item);+} => {
+        $(impl<T> $trait for Coordinates<T>
             where
                 T: $trait<Output=T>,
         {
             type Output = Self;
             $imp
-        }
+        })+
     };
-
 }
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Coordinates<T>(T, T);
 
-impl_op! {
+impl_ops! {
     Add => fn add(self, rhs: Coordinates<T>) -> Self::Output {
         Self(self.0 + rhs.0, self.1 + rhs.1)
-    }
-}
-impl_op! {
+    };
     Neg => fn neg(self                     ) -> Self::Output {
         Self(-self.0, -self.1)
-    }
-}
-impl_op! {
+    };
     Sub => fn sub(self, rhs: Coordinates<T>) -> Self::Output {
         Self(self.0 - rhs.0, self.1 - rhs.1)
-    }
-}
-impl_op! {
+    };
     Mul => fn mul(self, rhs: Coordinates<T>) -> Self::Output {
         Self(self.0 * rhs.0, self.1 * rhs.1)
-    }
-}
-impl_op! {
+    };
     Div => fn div(self, rhs: Coordinates<T>) -> Self::Output {
         Self(self.0 / rhs.0, self.1 / rhs.1)
-    }
+    }    
 }
+
 
 #[cfg(test)]
 mod tests {
